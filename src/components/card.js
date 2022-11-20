ade2ec9c9284f814810e1e2a2d677c884bffb79a
mika-israeli <mika80666@gmail.com>
2022-11-20 16:27:26 +0200

    change

--- src/components/card.js
+++ src/components/card.js
@@ -1,22 +1,26 @@
 import React from "react";
 import { Route } from "react-router-dom";
-const Card = ({ item}) => {
-    // const{name,price,img,title}=item;
-    
-    return (
-        <div className="cards">
-            <div className="details">
-                {/* <p>{title}</p> */}
-                <p>{item.name}</p>
-                <p>Price: {item.price}$</p>
-                <p><button onClick={(<Route path="/home/"/>)}>More Information</button></p>
-            </div>  
-            <div className="image_box">
-                <img src={item.img} alt="" />
-            </div>
-
-        </div>
-    );   
-
+const Card = ({ item }) => {
+  // const{name,price,img,title}=item;
+  function more() {
+    console.log(item);
+    const href = "/" + item.id;
+    <Route path={href} />;
+  }
+  return (
+    <div className="cards">
+      <div className="details">
+        {/* <p>{title}</p> */}
+        <p>{item.name}</p>
+        <p>Price: {item.price}$</p>
+        <p>
+          <button onClick={more}>More Information</button>
+        </p>
+      </div>
+      <div className="image_box">
+        <img src={item.img} alt="" />
+      </div>
+    </div>
+  );
 };
 export default Card;
--- src/components/home.js
+++ src/components/home.js
@@ -1,17 +1,16 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import Card from "./card";
 import products from "../data";
 import "../styles/home.css";
+import { Route } from "react-router-dom";
+
 const Home = () => {
-    return(
-        <section>
-
-            {products.map((item) => {
-                return <Card key={item.id} item={item} />
-            })}
-
-        
-        </section>
-    );
+  return (
+    <section>
+      {products.map((item) => {
+        return <Card key={item.id} item={item} />;
+      })}
+    </section>
+  );
 };
 export default Home;
