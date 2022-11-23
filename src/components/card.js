cc6d47f0e420b9da3e9c785eb8da892a70d61a65
mika-israeli <mika80666@gmail.com>
2022-11-23 12:50:49 +0200

    merge to the main

--- src/components/card.js
+++ src/components/card.js
@@ -1,25 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const Card = ({ item}) => {
-    // const{name,price,img,title}=item;
-    
-    return (
-        <div className="cards">
-            <div className="details">
-                
-                <p>{item.name}</p>
-                <p>Price: {item.price}$</p>
-                <Link to={`/${item.id}`}><button>Details</button></Link>
-                
-                
-                
-            </div>  
-            <div className="image_box">
-                <img src={item.img} alt="" />
-            </div>
+const Card = ({ item }) => {
+  // const{name,price,img,title}=item;
 
-        </div>
-    );   
-
+  return (
+    <div className="cards">
+      <div className="details">
+        <p>{item.name}</p>
+        <p>Price: {item.price}$</p>
+        <Link to={`/${item.id}`}>
+          <button>Details</button>
+        </Link>
+      </div>
+      <div className="image_box">
+        <img src={item.img} alt="" />
+      </div>
+    </div>
+  );
 };
 export default Card;
